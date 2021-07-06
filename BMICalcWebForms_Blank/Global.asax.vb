ca137dcfa8e1bfaa284ead12bb0231e69ebc60da
yokoyac <85912170+yokoyac@users.noreply.github.com>
2021-07-06 10:50:55 +0900

    Add files via upload

---
+++ BMICalcWebForms_Blank/Global.asax.vb
@@ -0,0 +1,16 @@
+﻿
+Public Class Global_asax
+    Inherits HttpApplication
+
+    Sub Application_Start(sender As Object, e As EventArgs)
+        ' アプリケーションの起動時に呼び出されます
+
+        ScriptManager.ScriptResourceMapping.AddDefinition("jquery",
+        New ScriptResourceDefinition() With
+        {
+           .Path = "http://ajax.microsoft.com/ajax/jQuery/jquery-1.10.2.min.js",
+           .DebugPath = "http://ajax.microsoft.com/ajax/jQuery/jquery-1.10.2.min.js"
+        }
+)
+    End Sub
+End Class
