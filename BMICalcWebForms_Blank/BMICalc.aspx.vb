e78d4ce048f29ae84accfd71a8a2b5c8132b9024
yokoyac <85912170+yokoyac@users.noreply.github.com>
2021-07-07 10:00:15 +0900

    コードの修正
    ・integerをdouble型に変更(小数も入力計算できるように)
    ・BMI、適正体重の表示を小数第2位までに
    ・returnで終了(if文の入れ子を少なくするため)

--- BMICalcWebForms_Blank/BMICalc.aspx.vb
+++ BMICalcWebForms_Blank/BMICalc.aspx.vb
@@ -12,24 +12,33 @@
     ''' <param name="e"></param>
     Protected Sub btnCalc_Click(sender As Object, e As EventArgs)
 
+
+        'If True Then
+        '    ShowMessage("未入力です")
+        '    Return
+        'End If
+
         ' 空文字判定
         If txtHeight.Text Is "" Or txtWeight.Text Is "" Then
 
             ShowMessage("未入力です")
+            Return
 
-        ElseIf IsNumeric(txtHeight.Text) And IsNumeric(txtWeight.Text) Then
+        End If
+
+        If IsNumeric(txtHeight.Text) And IsNumeric(txtWeight.Text) Then
 
             ' 身長(m)、体重(kg)
-            Dim height As Double = Integer.Parse(txtHeight.Text) / 100
-            Dim weight As Double = Integer.Parse(txtWeight.Text)
+            Dim height As Double = Double.Parse(txtHeight.Text) / 100
+            Dim weight As Double = Double.Parse(txtWeight.Text)
 
             ' 身長、体重が0以上のとき
             If height >= 0 And weight >= 0 Then
 
-                ' BMI
-                lblBMI.Text = weight / (height * height)
-                ' 適正体重
-                lblProperWeight.Text = (height * height) * 22
+                ' BMI(小数第2位まで)
+                lblBMI.Text = ((Math.Floor((weight / (height * height)) * 100)) / 100)
+                ' 適正体重(小数第2位まで)
+                lblProperWeight.Text = ((Math.Floor(((height * height) * 22) * 100)) / 100)
 
             Else
                 ShowMessage("0以上の数値を入力してください")
